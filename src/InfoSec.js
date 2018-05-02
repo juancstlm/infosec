49c9def3d4321ba7a01e44edb222c8fac9e65a95
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-05-01 20:13:00 -0700

    Add withrouter wrapper on infosec

--- src/InfoSec.js
+++ src/InfoSec.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './stylesheets/infosec.css';
 import Footer from "./components/Footer.js"
+import {withRouter} from "react-router-dom";
 
 class InfoSec extends Component {
     render() {
@@ -9,13 +10,10 @@ class InfoSec extends Component {
                 <header className="infosec-header">
                     <h1>Information Security</h1>
                 </header>
-                <div>
-
-                </div>
                 <Footer/>
             </div>
         );
     }
 }
 
-export default InfoSec;
+export default withRouter(InfoSec);
