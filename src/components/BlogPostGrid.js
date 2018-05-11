35f20721f520e78fc818ed8ae92544791d629de5
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-05-10 21:07:57 -0700

    Fetch all available from the database

--- src/components/BlogPostGrid.js
+++ src/components/BlogPostGrid.js
@@ -1,26 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import BlogPostCard from './BlogPostCard'
+import {DynamoDB} from "aws-sdk";
+
+var dynamodb
 
 export default class BlogPostGrid extends React.Component{
 
-    renderPostCards=()=>{
-        return (
-            this.props.blogPosts.map((post)=><BlogPostCard postid={post} key={post}/>)
-        )
+  constructor(){
+    super()
+    this.setKeys()
+    this.state = {
+      blogPosts: [],
+    }
+    this.getBlogPosts()
+  }
+
+  renderPostCards=()=>{
+    return (
+      this.state.blogPosts.map((post)=><BlogPostCard postid={post} key={post}/>)
+    )
+  }
+
+  getBlogPosts(){
+    var params ={
+      ExpressionAttributeNames:{
+        '#I': 'postid'
+      },
+      TableName: "infosecblog",
+      ProjectionExpression: "#I",
     }
 
-    render(){
-        return(
-            <div className='blog_post_grid'>
-                {this.renderPostCards()}
-            </div>
-        )
+    dynamodb.scan(params, (err, data)=>{
+      if(err) console.log(err, err.stack);
+      else {
+        data.Items.forEach((item)=>{
+          console.log(item.postid.S);
+          this.setState({
+            blogPosts: [...this.state.blogPosts, item.postid.S]
+          })
+        })
+      }
+    })
+  }
+
+  render(){
+    return(
+      <div className='blog_post_grid'>
+        {this.renderPostCards()}
+      </div>
+    )
+  }
+
+  setKeys(){
+    dynamodb = new DynamoDB({
+      region: require('../credentials').region,
+      credentials: {
+        accessKeyId: require('../credentials').accessKeyId,
+        secretAccessKey: require('../credentials').secretAccessKey,
+      }})
     }
-}
-BlogPostGrid.propTypes = {
-    blogPosts: PropTypes.array,
-}
-BlogPostGrid.defaultProps = {
-    blogPosts: ['1','2']
-}
+  }
